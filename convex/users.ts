aa1a5a7300efba9ccae324742029ad13e539fd1a
akash-parashar <aparashar508@gmail.com>
2024-09-08 03:26:14 +0530

    get group members query added

--- convex/users.ts
+++ convex/users.ts
@@ -39,23 +39,6 @@ export const updateUser = internalMutation({
   },
 });
 
-export const setUserOffline = internalMutation({
-  args: { tokenIdentifier: v.string() },
-  handler: async (ctx, args) => {
-    const user = await ctx.db
-      .query("users")
-      .withIndex("by_tokenIdentifier", (q) =>
-        q.eq("tokenIdentifier", args.tokenIdentifier)
-      )
-      .unique();
-
-    if (!user) {
-      throw new ConvexError("User not found");
-    }
-
-    await ctx.db.patch(user._id, { isOnline: false });
-  },
-});
 export const setUserOnline = internalMutation({
   args: { tokenIdentifier: v.string() },
   handler: async (ctx, args) => {
@@ -74,9 +57,27 @@ export const setUserOnline = internalMutation({
   },
 });
 
+export const setUserOffline = internalMutation({
+  args: { tokenIdentifier: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_tokenIdentifier", (q) =>
+        q.eq("tokenIdentifier", args.tokenIdentifier)
+      )
+      .unique();
+
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+
+    await ctx.db.patch(user._id, { isOnline: false });
+  },
+});
+
 export const getUsers = query({
   args: {},
-  handler: async (ctx) => {
+  handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
       throw new ConvexError("Unauthorized");
@@ -90,22 +91,50 @@ export const getUsers = query({
 });
 
 export const getMe = query({
-	args: {},
-	handler: async (ctx, args) => {
-		const identity = await ctx.auth.getUserIdentity();
-		if (!identity) {
-			throw new ConvexError("Unauthorized");
-		}
+  args: {},
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new ConvexError("Unauthorized");
+    }
 
-		const user = await ctx.db
-			.query("users")
-			.withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", identity.tokenIdentifier))
-			.unique();
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_tokenIdentifier", (q) =>
+        q.eq("tokenIdentifier", identity.tokenIdentifier)
+      )
+      .unique();
 
-		if (!user) {
-			throw new ConvexError("User not found");
-		}
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
 
-		return user;
-	},
+    return user;
+  },
+});
+
+export const getGroupMembers = query({
+  args: { conversationId: v.id("conversations") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const conversation = await ctx.db
+      .query("conversations")
+      .filter((q) => q.eq(q.field("_id"), args.conversationId))
+      .first();
+    if (!conversation) {
+      throw new ConvexError("Conversation not found");
+    }
+
+    const users = await ctx.db.query("users").collect();
+    const groupMembers = users.filter((user) =>
+      conversation.participants.includes(user._id)
+    );
+
+    return groupMembers;
+  },
 });
--- src/components/home/group-members-dialog.tsx
+++ src/components/home/group-members-dialog.tsx
@@ -1,4 +1,4 @@
-import { users } from "@/dummy-data/db";
+// import { users } from "@/dummy-data/db";
 import {
   Dialog,
   DialogContent,
@@ -9,8 +9,17 @@ import {
 } from "@/components/ui/dialog";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Crown } from "lucide-react";
+import { Conversation } from "@/store/chat-store";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
-const GroupMembersDialog = () => {
+type GroupMembersDialogProps = {
+  selectedConversation:Conversation;
+}
+
+const GroupMembersDialog = ({selectedConversation}:GroupMembersDialogProps) => {
+  const users = useQuery(api.users.getGroupMembers,{conversationId:selectedConversation._id})
+  
   return (
     <Dialog>
       <DialogTrigger>
@@ -44,7 +53,8 @@ const GroupMembersDialog = () => {
                       <h3 className="text-md font-medium">
                         {user.name || user.email.split("@")[0]}
                       </h3>
-                      {user.admin && (
+                      {user._id  === selectedConversation.admin && (
+
                         <Crown size={16} className="text-yellow-400" />
                       )}
                     </div>
